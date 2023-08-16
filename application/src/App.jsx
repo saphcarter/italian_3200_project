a33ade36fdfc3d9f27f003e2148220f5a50b7920
saphcarter <sapphire@vitruvian.me>
2023-08-16 13:56:37 +0800

    trying to set up flask

--- application/src/App.jsx
+++ application/src/App.jsx
@@ -1,12 +1,44 @@
 import "./styles/App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 import NavbarComponent from "./Components/Navbar";
 
 function App() {
+  const [data, setdata] = useState({
+    name: "",
+    age: 0,
+    date: "",
+    programming: "",
+  });
+
+  // Using useEffect for single rendering
+  useEffect(() => {
+    // Using fetch to fetch the api from
+    // flask server it will be redirected to proxy
+    fetch("/data").then((res) =>
+      res.json().then((data) => {
+        // Setting a data from api
+        setdata({
+          name: data.Name,
+          age: data.Age,
+          date: data.Date,
+          programming: data.programming,
+        });
+      })
+    );
+  }, []);
+
   return (
-    <>
+    <div className="App">
       <NavbarComponent />
-      <div>Hello</div>{" "}
-    </>
+      <header className="App-header">
+        <h1>React and flask</h1>
+        <p>{data.name}</p>
+        <p>{data.age}</p>
+        <p>{data.date}</p>
+        <p>{data.programming}</p>
+      </header>
+    </div>
   );
 }
 
