38a2d8855f33ea7346b62fc17d4620ec2f8d1ba9
saphcarter <sapphire@vitruvian.me>
2023-08-16 15:14:51 +0800

    set up websockets with vite to access flask

--- application/vite.config.js
+++ application/vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/time": {
+        target: "ws://127.0.0.1:5000/",
+        ws: true,
+      },
+      "/api": {
+        target: "http://127.0.0.1:5000/",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
+      },
+    },
+  },
+});
