a4f7f23583ea4a5e0c7b024a3c82f8d62c916388
saphcarter <sapphire@vitruvian.me>
2023-08-16 14:16:49 +0800

    still setting up flask

--- application/backend/server.py
+++ application/backend/server.py
@@ -1,24 +1,12 @@
-# Import flask and datetime module for showing date and time
-from flask import Flask
-import datetime
- 
-x = datetime.datetime.now()
- 
-# Initializing flask app
-app = Flask(__name__)
- 
 # Route for seeing a data
-@app.route('/data')
-def get_time():
- 
-    # Returning an api for showing in  reactjs
-    return {
-        'Name':"geek",
-        "Age":"22",
-        "Date":x,
-        "programming":"python"
-        }
- 
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
      
 # Running app
 if __name__ == '__main__':
--- application/src/App.jsx
+++ application/src/App.jsx
@@ -4,39 +4,22 @@ import { useEffect } from "react";
 import NavbarComponent from "./Components/Navbar";
 
 function App() {
-  const [data, setdata] = useState({
-    name: "",
-    age: 0,
-    date: "",
-    programming: "",
-  });
+  const [currentTime, setCurrentTime] = useState(0);
 
-  // Using useEffect for single rendering
   useEffect(() => {
-    // Using fetch to fetch the api from
-    // flask server it will be redirected to proxy
-    fetch("/data").then((res) =>
-      res.json().then((data) => {
-        // Setting a data from api
-        setdata({
-          name: data.Name,
-          age: data.Age,
-          date: data.Date,
-          programming: data.programming,
-        });
-      })
-    );
+    fetch("/time")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentTime(data.time);
+      });
   }, []);
 
   return (
     <div className="App">
       <NavbarComponent />
       <header className="App-header">
-        <h1>React and flask</h1>
-        <p>{data.name}</p>
-        <p>{data.age}</p>
-        <p>{data.date}</p>
-        <p>{data.programming}</p>
+        <h1>Testing</h1>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
