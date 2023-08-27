08b70ab38ff72b3ef115a92cc18f55c35ffe996b
saphcarter <sapphire@vitruvian.me>
2023-08-27 13:10:52 +0800

    trying to get gtihub actions to work

--- application/vite.config.js
+++ application/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/italian_3200_project/",
   server: {
     proxy: {
       "/time": {
