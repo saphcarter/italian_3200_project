6f91b3f85da9e803424d85216a6be6c10e1d18d3
saphcarter <sapphire@vitruvian.me>
2023-09-07 15:21:10 +0800

    some updates

--- application/backend/algo.py
+++ application/backend/algo.py
@@ -2,16 +2,21 @@ import librosa
 import audiosegment
 import numpy as np
 import sox
+import matplotlib.pyplot as plt
+from pydub import AudioSegment
 
+# had to brew install sox and ffmpeg
 # note: sox not directly used but needed for filter_silence()... just a reminder for now
 
 # (BOTH METHODS) filler name/path of audio files
 question_file = "question.m4a"
 answer_file = "answer.m4a"
+static_file = "static.m4a"
 
 # (BOTH METHODS) read in audio files using audiosegment (from pydub)
 question = audiosegment.from_file(question_file)
 answer = audiosegment.from_file(answer_file)
+static = audiosegment.from_file(static_file)
 
 print("Loaded Files Done...")
 
@@ -21,6 +26,46 @@ print("Loaded Files Done...")
 # OR... other processing here maybe
 f_question = question.filter_silence()
 f_answer = answer.filter_silence()
+
+fig, ax = plt.subplots(3, 1)
+
+#no filtering
+y, sr = librosa.load(question_file, mono=False, duration=10)
+ytrim, index = librosa.effects.trim(y)
+librosa.display.waveshow(ytrim, sr=sr, ax=ax[0])
+ax[0].set(title='question')
+ax[0].label_outer()
+
+# with filtering
+y2, sr2 = librosa.load(answer_file, mono=False, duration=10)
+ytrim2, index = librosa.effects.trim(y2, top_db = 1000)
+
+
+# Create an AudioSegment object from the NumPy array
+audio = AudioSegment(
+    ytrim2.tobytes(),  # Convert the array to bytes
+    frame_rate=sr2,
+    sample_width=ytrim2.dtype.itemsize,
+    channels=1,  # Mono audio
+)
+
+# Optional: Save the audio to a file (e.g., "output.wav")
+audio.export("output.wav", format="wav")
+
+
+librosa.display.waveshow(ytrim2, sr=sr2, ax=ax[2])
+ax[2].set(title='answer trimed')
+ax[2].label_outer()
+
+plt.ylim(-0.5, 0.5)
+
+librosa.display.waveshow(y, sr=sr2, ax=ax[1])
+ax[1].set(title='answer no trim')
+ax[1].label_outer()
+
+plt.show()
+
+result = y-y2
 
 # compute spectrogram of audio files
 freqs1, times1, amplitudes1 = f_question.spectrogram(window_length_s=0.03, overlap=0.5)
@@ -38,13 +83,15 @@ answer_arr = answer.get_array_of_samples()
 answer_arr = np.array(answer_arr)
 
 # compute similarity matrix of audio files
-cross_similarity_matrix = librosa.segment.cross_similarity(answer_arr, question_arr)
+'''commented out because of time'''
+# cross_similarity_matrix = librosa.segment.cross_similarity(answer_arr, question_arr)
 
 # TODO: ANALYSE THE MATRIX TO GIVE A SCORE SOMEHOW
 # MAYBE librosa.sequence.rqa????
 
 # calculate alignment score matrix
-alignment_matrix = librosa.sequence.rqa(cross_similarity_matrix)
+'''commented out because of time'''
+# alignment_matrix = librosa.sequence.rqa(cross_similarity_matrix)
 # ^^^ i think this might be good but still have to process the result a bit more i think
 # possibly get the value in the bottom right corner (ie column.length-1 & row.length-1)????
 # then compare to values based on testing
