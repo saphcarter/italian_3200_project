80367aa8fa493f8862f02b61a3c903beda595b2a
saphcarter <sapphire@vitruvian.me>
2023-09-11 19:53:43 +0800

    styling log on

--- application/src/components/LoginButton.jsx
+++ application/src/components/LoginButton.jsx
@@ -1,14 +1,46 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+const AuthenticationButton = () => {
+  const { isAuthenticated } = useAuth0();
 
-    return (
-        <button onClick={() => loginWithRedirect()}>
-            Log In
-        </button>
-    );
-}
+  return isAuthenticated ? <LogoutButton /> : <LoginButton />;
+};
 
-export default LoginButton;
+export const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/profile",
+      },
+    });
+  };
+
+  return (
+    <button className="auth0-button" onClick={handleLogin}>
+      Log In
+    </button>
+  );
+};
+
+export const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  const handleLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  };
+
+  return (
+    <button className="auth0-button" onClick={handleLogout}>
+      Log Out
+    </button>
+  );
+};
+
+export default AuthenticationButton;
