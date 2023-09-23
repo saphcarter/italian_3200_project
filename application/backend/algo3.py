71c8e8f6be94c8383ef7d7f71ae8c2eaefccec2b
Tom McAdam <76895458+tom-mcadam@users.noreply.github.com>
2023-09-23 13:38:41 +0800

    add dtw factor

--- application/backend/algo3.py
+++ application/backend/algo3.py
@@ -165,7 +165,18 @@ def get_mfcc(audio, rate):
     mfcc = librosa.feature.mfcc(y=audio, sr=rate, n_mfcc=13)
     return mfcc
 
+def normalise_mfcc(mfcc):
+    mfcc -= 0.7 if (mfcc-0.7) > 0 else 0
+    mfcc /= 0.25 if (mfcc-0.7) <= 1 else 1
+    return mfcc
+
+def dtw_to_percent(dtw_distance):
+    return (1 / (1 + dtw_distance)) * 100
+
 def compareFiles(path1, path2):
+    audio1, rate1 = trimFile(path1)
+    audio2, rate2 = trimFile(path2)
+
     audio1, rate1 = trimFile(path1)
     audio2, rate2 = trimFile(path2)
 
@@ -193,40 +204,34 @@ def compareFiles(path1, path2):
     # process audio 1
     b, a = signal.butter(10, [f_low, f_high], btype='bandpass', fs=rate1)
     filt_audio1 = signal.lfilter(b, a, audio1)
-    
     # Convert to decibel spectogram
     spectogram1 = np.abs(librosa.stft(filt_audio1))
     dbSpectogram1 = librosa.amplitude_to_db(spectogram1, ref=np.max)
-   
     # make recurrence matrix for non-median filtering
     rec1 = librosa.segment.recurrence_matrix(dbSpectogram1, mode='affinity', metric='cosine', sparse=True)
-    
     # filter spectogram to reduce noise
     spectogram_filter1 = np.minimum(dbSpectogram1, librosa.decompose.nn_filter(dbSpectogram1, rec=rec1, aggregate=np.average, metric='cosine'))
 
     # process audio 2
     b, a = signal.butter(10, [f_low, f_high], btype='bandpass', fs=rate2)
     filt_audio2 = signal.lfilter(b, a, audio2)
-    
     # Convert to decibel spectogram
     spectogram2 = np.abs(librosa.stft(filt_audio2))
     dbSpectogram2 = librosa.amplitude_to_db(spectogram2, ref=np.max)
-     
     # make recurrence matrix for non-median filtering
     rec2 = librosa.segment.recurrence_matrix(dbSpectogram2, mode='affinity', metric='cosine', sparse=True)
-    
     # filter spectogram to reduce noise
     spectogram_filter2 = np.minimum(dbSpectogram2, librosa.decompose.nn_filter(dbSpectogram2, rec=rec2, aggregate=np.average, metric='cosine'))
 
     # SCORE FACTOR ONE: TIMING & RHYTHM SCORE
-    # subfactor one --------- spectrogram correlation -----------
+    # sub-factor one --------- spectrogram correlation -----------
 
     flat_d1 = spectogram_filter1.flatten()
     flat_d2 = spectogram_filter2.flatten()
 
     pearson_correlation = np.corrcoef(flat_d1, flat_d2)[0, 1]
 
-    # subfactor two --------- onset detection & analysis -----------
+    # sub-factor two --------- onset detection & analysis -----------
 
     onset1 = librosa.onset.onset_detect(y=audio1, sr=rate1)
     onset2 = librosa.onset.onset_detect(y=audio2, sr=rate2)
@@ -235,26 +240,29 @@ def compareFiles(path1, path2):
     estimated_iou = calculate_iou_with_tolerance(onset1, onset2, tolerance_threshold)
 
     # SCORE FACTOR TWO: FREQUENCY SCORE
-    # subfactor one --------- mel-frequency cepstral coefficients -----------
+    # sub-factor one --------- mel-frequency cepstral coefficients -----------
 
     mfcc1 = get_mfcc(audio1, rate1)
     mfcc2 = get_mfcc(audio2, rate2)
 
     cosine_distance = 1 - distance.cosine(mfcc1.flatten(), mfcc2.flatten())
-    mfcc_score = cosine_distance
+    mfcc_score = normalise_mfcc(cosine_distance)
 
-    # EITHER: MODIFY MFCC SCORE TO BE STRICTER... OR ONE OTHER FACTOR HERE
-    # TEST ON BLANK FILE.... NOISE.... ETC
+    # sub-factor two --------- dynamic time warping -----------
+    
+    distance, path = fastdtw(audio1, audio2, dist=euclidean)
+    dtw_score = (1 / (1 + distance)) * 100
 
     # --------- RESULTS -----------
 
     rhythm_score = (pearson_correlation + estimated_iou) / 2
-    intonation_score = mfcc_score
+    intonation_score = (mfcc_score + dtw_score) / 2 
     combined_score = (rhythm_score + intonation_score) / 2
 
     print(f"Pearson Correlation Coefficient: {pearson_correlation}")
     print(f"Onset Correlation Factor: {estimated_iou}")
     print(f"Mel-frequency Cepstral Coefficients Score: {mfcc_score}")
+    print(f"Dynamic time warping Score: {dtw_score}")
     print(f"--------------------------------------------------------------------------")
     print(f"--------------------------------------------------------------------------")
     print(f"RHYTHM SCORE: {rhythm_score}")
@@ -263,7 +271,7 @@ def compareFiles(path1, path2):
     print(f"--------------------------------------------------------------------------")
     print(f"OVERALL SCORE: {combined_score*100:.1f}%")
 
-
+    return rhythm_score, intonation_score, combined_score
 
 def main():
     compareFiles('./answer.m4a', './static.m4a')
