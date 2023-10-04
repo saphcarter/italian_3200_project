aa188b664d7163ab034ade9322bc5590447c9099
Tom McAdam <76895458+tom-mcadam@users.noreply.github.com>
2023-10-04 09:58:10 +0800

    add task form

--- application/src/components/TaskAddForm.jsx
+++ application/src/components/TaskAddForm.jsx
@@ -1,97 +1,130 @@
 import React, { useState } from 'react';
 
 const TaskAddForm = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [numOfQuestions, setNumOfQuestions] = useState(1);
-  const [questions, setQuestions] = useState([]);
+    const [dueDateTime, setDueDateTime] = useState('');
+    const [warning, setWarning] = useState('');
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [numOfQuestions, setNumOfQuestions] = useState(1);
+    const [questions, setQuestions] = useState([]);
 
-  const OpenPopup = () => {
-    setIsPopupOpen(true);
-  };
+    const OpenPopup = () => {
+        setIsPopupOpen(true);
+    };
 
-  const ClosePopup = () => {
-    setIsPopupOpen(false);
-  };
+    const ClosePopup = () => {
+        setIsPopupOpen(false);
+    };
 
-  const Submit = (e) => {
-    e.preventDefault();
+    const Submit = (e) => {
+        e.preventDefault();
 
-    // Process the form data (e.g., send it to a server, update state, etc.)
+        if (warning) {
+            alert('Please select a future date and time.');
+            return;
+        }
 
-    // Reset the form inputs (if needed)
+        // Process the form data (e.g., send it to a server, update state, etc.)
 
-    // Close the popup
-    setIsPopupOpen(false);
-  };
+        // Reset the form inputs (if needed)
 
-  const QuestionChange = (index, e) => {
-    const { name, value } = e.target;
-    const updatedQuestions = [...questions];
-    updatedQuestions[index][name] = value;
-    setQuestions(updatedQuestions);
-  };
+        // Close the popup
+        setIsPopupOpen(false);
+    };
 
-  return (
-    <div className="section">
-      <h2>Task Manager</h2>
+    const QuestionChange = (index, e) => {
+        const { name, value } = e.target;
+        const updatedQuestions = [...questions];
+        updatedQuestions[index][name] = value;
+        setQuestions(updatedQuestions);
+    };
 
-      {!isPopupOpen && (
-        <button onClick={OpenPopup}>Add New Task </button>
-      )}
+    const handleDateChange = (e) => {
+        const selectedDateTime = new Date(e.target.value);
+        const currentDateTime = new Date();
+    
+        if (selectedDateTime < currentDateTime) {
+          setWarning('Warning: This date and time has already passed. Please select a date and time in the future.');
+        } else {
+          setWarning('');
+        }
+    
+        setDueDateTime(e.target.value);
+    };
 
-      {isPopupOpen && (
-        <div className="popup">
-          <div className="popup-content">
-            <button className="form-group close" onClick={ClosePopup}>
-              Cancel Task &times;
-            </button>
+    return (
+        <div className="section">
+            <h2>Task Manager</h2>
 
-            <form onSubmit={Submit}>
-              <div className="form-group">
-                <label style={{ marginRight: '8px' }} htmlFor="taskName">Task Name: </label>
-                <input
-                    type="text"
-                    id="taskName"
-                    placeholder="e.g. Week 1 Quiz"
-                    required />
-              </div>
+            {!isPopupOpen && (
+                <button onClick={OpenPopup}>Add New Task </button>
+            )}
 
-              <div className="form-group">
-                <label style={{ marginRight: '8px' }} htmlFor="numOfQuestions">Number of Questions: </label>
-                <select
-                  id="numOfQuestions"
-                  value={numOfQuestions}
-                  onChange={(e) => setNumOfQuestions(e.target.value)}
-                  required
-                >
-                  <option value={1}>1</option>
-                  <option value={2}>2</option>
-                  <option value={3}>3</option>
-                  <option value={4}>4</option>
-                  <option value={5}>5</option>
-                </select>
-              </div>
+            {isPopupOpen && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <button className="form-group close" onClick={ClosePopup}>
+                            Cancel Task &times;
+                        </button>
 
-              {Array.from({ length: numOfQuestions }).map((_, index) => (
-                <div className="form-group" key={index}>
-                  <label style={{ marginRight: '8px' }}>Question {index + 1}:</label>
-                  <input
-                    type="file"
-                    name="questionFile"
-                    accept="audio/*"
-                    onChange={(e) => QuestionChange(index, e)}
-                    required
-                  />
+                    <form onSubmit={Submit}>
+                        <div className="form-group">
+                            <label style={{ marginRight: '8px' }} htmlFor="taskName">Task Name: </label>
+                            <input
+                                type="text"
+                                id="taskName"
+                                placeholder="e.g. Week 1 Quiz"
+                                required />
+                        </div>
+
+                        <div className="form-group">
+                            <label style={{ marginRight: '8px' }} htmlFor="dueDateTime">Due Date and Time:</label>
+                                <input
+                                    type="datetime-local"
+                                    id="dueDateTime"
+                                    name="dueDateTime"
+                                    onChange={handleDateChange}
+                                    defaultValue={`${new Date().toISOString().slice(0,10)}T23:59`}
+                                    required
+                                />
+                                {warning && <div className="warning">{warning}</div>}
+                        </div>
+
+                        <div className="form-group">
+                            <label style={{ marginRight: '8px' }} htmlFor="numOfQuestions">Number of Questions: </label>
+                            <select
+                                id="numOfQuestions"
+                                value={numOfQuestions}
+                                onChange={(e) => setNumOfQuestions(e.target.value)}
+                                required
+                            >
+                                <option value={1}>1</option>
+                                <option value={2}>2</option>
+                                <option value={3}>3</option>
+                                <option value={4}>4</option>
+                                <option value={5}>5</option>
+                            </select>
+                        </div>
+
+                        {Array.from({ length: numOfQuestions }).map((_, index) => (
+                            <div className="form-group" key={index}>
+                                <label style={{ marginRight: '8px' }}>Question {index + 1}:</label>
+                                <input
+                                    type="file"
+                                    name="questionFile"
+                                    accept="audio/*"
+                                    onChange={(e) => QuestionChange(index, e)}
+                                    required
+                                />
+                            </div>
+                        ))}
+
+                        <button className="form-group" type="submit">Add Task</button>
+                    </form>
                 </div>
-              ))}
-
-              <button className="form-group" type="submit">Add Task</button>
-            </form>
-          </div>
+            </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default TaskAddForm;
