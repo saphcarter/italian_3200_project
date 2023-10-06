4de314a705badbb2d2bc3fea004329c27294d1de
SamWooday12 <89955679+SamWooday12@users.noreply.github.com>
2023-10-06 13:35:41 +0800

    Add files via upload

--- application/backend/server.py
+++ application/backend/server.py
@@ -1,5 +1,20 @@
-from backend import app
+import os
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-# Running app
-if __name__ == '__main__':
-    app.run(debug=True)
+# because this file is called server.py, when using flask tutorials you 
+# may have to replace the word app with server when making calls in terminal
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from backend import routes
+
+from routes2 import *
