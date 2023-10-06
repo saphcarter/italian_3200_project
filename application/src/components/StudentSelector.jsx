22817378686ba4845c3dcefbb2723eb1c84600ce
Tom McAdam <76895458+tom-mcadam@users.noreply.github.com>
2023-10-06 09:03:33 +0800

    admin results template

---
+++ application/src/components/StudentSelector.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import ScoreSection from './Results';
+
+function ScorePopup({ onClose, student_full_name }) {
+    return (
+        <div className="score-popup">
+            <button className="close-button" onClick={onClose}>Close</button>
+            <ScoreSection name={student_full_name} />
+        </div>
+    );
+}
+
+function StudentCard({full_name, student_number}) {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const openPopup = () => {
+        setIsPopupOpen(true);
+    };
+
+    const closePopup = () => {
+        setIsPopupOpen(false);
+    };
+    
+    return (
+        <>
+            <div className="score-card" onClick={openPopup}>
+                <h4 className="score-text">{full_name}</h4>
+                <p className="score-text">Student Number: {student_number}</p>
+            </div>
+            {isPopupOpen && (
+                <ScorePopup onClose={closePopup} student_full_name={full_name} />
+            )}
+        </>
+    );
+  }
+
+function StudentSelector() {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        // Handle the search logic here, e.g., make an API call
+        console.log('Search term:', searchTerm);
+    };
+
+    return (
+        <div className="section">
+            <form  onSubmit={handleSearchSubmit}>
+                <input 
+                    style= {{ marginRight: '8px' }}
+                    type="text"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    placeholder="Search for a student..."
+                />
+                <button type="submit">Search</button>
+            </form>
+
+            <div style = {{ marginTop : "16px" }} className = "score-card-section">
+                <StudentCard full_name="Jane Doe" student_number = "123" />
+                <StudentCard full_name="John Doe" student_number = "456" />
+            </div>
+        </div>
+    );
+}
+
+export default StudentSelector;
