51a72abf6cb7d883d38a17111227f09bf6754845
Tom McAdam <76895458+tom-mcadam@users.noreply.github.com>
2023-10-11 14:20:26 +0800

    admin set up

--- application/src/pages/results.jsx
+++ application/src/pages/results.jsx
@@ -2,17 +2,17 @@ import "../styles/App.css";
 import ScoreSection from "../components/Results";
 import React, { useState, useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
+import StudentSelector from "../components/StudentSelector";
 
 function Results() {
   const { user, getIdTokenClaims } = useAuth0();
   const user_id = user?.sub;
   const [isAdmin, setIsAdmin] = useState(false);
 
-  
   useEffect(() => {
     const fetchClaims = async () => {
       const claims = await getIdTokenClaims();
-      const roles = claims['https://localhost:5173/roles'];
+      const roles = claims['https://learnitalianpronunciation.com/roles'];
       setIsAdmin(roles && roles.includes('admin'));
     };
     
