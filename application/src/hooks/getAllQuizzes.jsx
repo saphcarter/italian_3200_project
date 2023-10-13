54f5e067fdae5ae3f81bde77667fffcd60e8fcb4
saphcarter <sapphire@vitruvian.me>
2023-10-13 15:03:49 +0800

    Revert "supabase integrarion"
    This reverts commit 41dc70b3122b6d972e5e296c25ab19749e1e25ac.

--- application/src/hooks/getAllQuizzes.jsx
+++ application/src/hooks/getAllQuizzes.jsx
@@ -1,28 +1,11 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-
 function getAllQuizzes() {
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient({ cookies: () => cookieStore });
-
-  supabase
-    .from("quizzes")
-    .select()
-    .then(({ data: quizzes }) => {
-      return quizzes;
-    })
-    .catch((error) => {
-      // Handle any errors that occur during the request
-      console.error("Error fetching data:", error);
+  const quizzes = fetch("/quizzes")
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
     });
 
-  // const quizzes = fetch("/quizzes")
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     return data;
-  //   });
-
-  // return quizzes;
+  return quizzes;
 }
 
 export default getAllQuizzes;
