5600d1f22c9d0705c5c3817fac77217991bfe046
Tom McAdam <76895458+tom-mcadam@users.noreply.github.com>
2023-10-13 20:29:20 +0800

    Create quizzes.py

---
+++ application/api/quizzes.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler
+import json
+from ..backend.server import db
+from ..backend.models import Quiz
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        quizzes = Quiz.query.all()
+        quiz_names = []
+        for quiz in quizzes:
+            quiz_details = []
+            quiz_details.append(quiz.id)
+            quiz_details.append(quiz.name)
+            quiz_details.append(quiz.due_date)
+            quiz_names.append(quiz_details)
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(quiz_names).encode())
