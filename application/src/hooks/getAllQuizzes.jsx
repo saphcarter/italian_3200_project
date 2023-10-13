41dc70b3122b6d972e5e296c25ab19749e1e25ac
saphcarter <sapphire@vitruvian.me>
2023-10-13 15:03:21 +0800

    supabase integrarion

--- application/src/hooks/getAllQuizzes.jsx
+++ application/src/hooks/getAllQuizzes.jsx
@@ -1,11 +1,28 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
 function getAllQuizzes() {
-  const quizzes = fetch("/quizzes")
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+
+  supabase
+    .from("quizzes")
+    .select()
+    .then(({ data: quizzes }) => {
+      return quizzes;
+    })
+    .catch((error) => {
+      // Handle any errors that occur during the request
+      console.error("Error fetching data:", error);
     });
 
-  return quizzes;
+  // const quizzes = fetch("/quizzes")
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     return data;
+  //   });
+
+  // return quizzes;
 }
 
 export default getAllQuizzes;
